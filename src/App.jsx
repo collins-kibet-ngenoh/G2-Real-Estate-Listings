a76b48487bbf645deae15a0ec5a1c35e0cef15a8
collins-kibet-ngenoh <collins.kibet.ngenoh@gmail.com>
2024-05-07 19:42:34 +0300

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import About from './components/About'
+import PopularCategory from './components/PopularCategory'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -32,4 +34,4 @@ function App() {
   )
 }
 
-export default App
+export default App;
